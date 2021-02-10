70bc470fa3d53a1dcce019f5ee31c04e2a1881bc
jmurphy94 <jeffmurphy94@gmail.com>
2021-02-10 15:21:23 -0600

    Initial Upload
    V1.0 Initial upload.

---
+++ addons/PBC_ACE_Med/config.cpp
@@ -0,0 +1,43 @@
+class CfgPatches {
+    class PBC_ACE_Med {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "ace_medical_treatment"
+        };
+        author = "pbcrazy";
+        authors[] = {"pbcrazy"};
+    };
+};
+
+class ace_medical_replacementItems {
+    ItemType_401[] = {
+        {"ACE_fieldDressing", 2},
+		{"ACE_packingBandage", 4},
+		{"ACE_elasticBandage", 4},
+		{"ACE_quikClot", 1},
+        {"ACE_morphine", 2},
+		{"ACE_epinephrine", 1},
+        {"ACE_tourniquet", 2},
+		{"ACE_splint", 1}
+    };
+    ItemType_619[] = {
+        {"ACE_fieldDressing", 6},
+        {"ACE_packingBandage", 10},
+		{"ACE_elasticBandage", 10},
+		{"ACE_quikClot", 4},
+        {"ACE_epinephrine", 4},
+        {"ACE_morphine", 8},
+		{"ACE_adenosine", 2},
+        {"ACE_salineIV_500", 4},
+		{"ACE_salineIV_250", 2},
+        {"ACE_tourniquet", 4},
+        {"ACE_splint", 2},
+		{"ACE_surgicalKit", 1},
+		{"ACE_bodyBag", 2}
+    };
+    ACE_atropine[] = {
+        {"ACE_adenosine", 1}
+    };
+};
---
+++ mod.cpp
@@ -0,0 +1,22 @@
+/// Name of your mod
+name = "Crab ACE Medical";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "picture.png"; 
+/// Display next to the item added by the mod
+logoSmall = "logo_small.paa";
+/// Logo displayed in the main menu
+logo = "logo.paa";
+/// Change the logo When the mouse is over, in the main menu of Arma 3
+logoOver = "logo.paa";
+/// Website URL, that can accessed from the mod data
+action = "https://community.bistudio.com/wiki/Mod_Presentation";
+/// Tool tip displayed when the mouse is left over, in the main menu of Arma 3
+tooltipOwned = "What website?!";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] = {	0.23, 0.39, 0.30, 1};
+/// Overview text, displayed from the extension menu
+overview = "A mod to change the vanilla medical item conversions.";
+/// Hide the extension name 
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
